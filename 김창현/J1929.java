df2e7cb08f95670e1b467848db366836eed13d4f
[CHyunk] <[airplch@naver.com]>
2023-10-12 19:59:25 +0900

    Feat: 2주차 문제풀이

---
+++ 김창현/J1929.java
@@ -0,0 +1,13 @@
+package 김창현;
+
+import java.util.Scanner;
+
+public class hello {
+    public static void main (String[] args)  {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello");
+
+        in.close();
+    }
+}
--- 김창현/hello.java
+++
@@ -1,6 +0,0 @@
-package 김창현;
-
-public class hello {
-    System.out.printf("Hello");
-    
-}
