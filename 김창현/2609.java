b7641b1db3714236c3b02b375cb80496f193f272
CHyunk <145246039+CHyunk@users.noreply.github.com>
2023-10-12 19:50:06 +0900

    Create 2609.java

---
+++ 김창현/2609.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int GCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static int LCM(int a, int b) {
+        return (a * b / GCD(a, b));
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int gcdResult = GCD(a, b);
+        int lcmResult = LCM(a, b);
+
+        System.out.println(gcdResult);
+        System.out.println(lcmResult);
+    }
+}
